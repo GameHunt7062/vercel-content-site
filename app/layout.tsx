3cf7ccbed17ae8cfd9131e6302605440a4c2c911
GameHunt7062 <jmd700089@gmail.com>
2026-09-06 23:30:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {<link rel="stylesheet" href="https://use.typekit.net/pmo6wwa.css">}
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
